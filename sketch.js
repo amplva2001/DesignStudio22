25b816efe2386d108e7b4741a973e91cb9824ba8
amplva2001 <you@example.com>
2025-10-15 16:13:33 +0900

    change bg

--- sketch.js
+++ sketch.js
@@ -1,4 +1,13 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background('#9b1a1aff'); // or your color
+}
+
+function draw() {
+  // your draw code
+}
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  background('#9b1a1aff');
+  background('#9b1a1aff'); // or your color
 }
