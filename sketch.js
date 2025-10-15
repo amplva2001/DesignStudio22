7e921328498c6269b4fa6d3a01fe056443388cc5
amplva2001 <you@example.com>
2025-10-15 17:19:32 +0900

    gameboy picture upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,3 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0); // black
-}
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  background(0);
 }
