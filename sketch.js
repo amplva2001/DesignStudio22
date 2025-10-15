66799fc60f7806c786f2e2c58954197e9a5c3dbf
amplva2001 <you@example.com>
2025-10-15 15:14:02 +0900

    background 2

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  background(#000000);
+  createCanvas(windowWidth, windowHeight);
+  background(255);
 }
-
 function draw() {
   ellipse(width/2, height/2, 100, 100);
 }
