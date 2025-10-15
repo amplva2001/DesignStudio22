f73518e219b80c5c05cb35c9290b271f5789ec49
amplva2001 <you@example.com>
2025-10-15 17:37:26 +0900

    top bar fade

--- sketch.js
+++ sketch.js
@@ -1,3 +1,30 @@
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noLoop(); // We only need to draw this once
+  drawDecorativeBar();
+}
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  drawDecorativeBar();
+}
+
+function drawDecorativeBar() {
+  // Array of colors from top to bottom (sampled from your image, adjust as needed)
+  let colors = [
+    '#f8f58d',
+    '#f5f47a',
+    '#ebe45e',
+    '#d1c54f',
+    '#b6a63e',
+    '#998631',
+    '#7d6926'
+  ];
+  let barHeight = 20; // Height of each color bar
+
+  for (let i = 0; i < colors.length; i++) {
+    fill(colors[i]);
+    noStroke();
+    rect(0, i * barHeight, width, barHeight);
+  }
 }
